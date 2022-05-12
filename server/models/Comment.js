6735335f41255b37a4492622b9a569444dee9fbc
Noah Chernet <noahchernet@yahoo.com>
2022-05-12 13:48:17 +0300

    Updated comment schema to make sure all fields are entered and there's a timestamp

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 
-export const commentSchema = mongoose.Schema({
-  ticketId: mongoose.Types.ObjectId,
-  postedByUser: mongoose.Types.ObjectId,
-  description: String,
-});
+export const commentSchema = mongoose.Schema(
+  {
+    ticketId: { type: mongoose.Types.ObjectId, required: true },
+    postedByUser: { type: mongoose.Types.ObjectId, required: true },
+    description: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 export const Comment = mongoose.model("Comment", commentSchema);

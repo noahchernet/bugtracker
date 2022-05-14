bc3cdf24471ea513eb2afdada83fcf11c8c2ed2d
Noah Chernet <noahchernet@yahoo.com>
2022-05-14 10:04:46 +0300

    Added docs to all functions Started working on updateTicket function in controllers/Ticket.js

--- server/controllers/Ticket.js
+++ server/controllers/Ticket.js
@@ -1,5 +1,10 @@
 const Ticket = require("../models/Ticket.js");
 
+/**
+ * Get all or some tickets based on the query parameters
+ * @param {*} req Empty or contains title, severity and/or solved parameter values
+ * @param {*} res List of all tickets in database or selected tickets based on query parameters
+ */
 exports.getTickets = (req, res) => {
   const { title, severity, solved } = req.query;
   const conditions = {};
@@ -30,6 +35,12 @@ exports.getTickets = (req, res) => {
     );
 };
 
+/**
+ * Create a new ticket.
+ * @param {*} req Contians the title, description and severity of the ticket
+ * @param {*} res If the ticket is  created successfully, the body of res
+ * contains the newly created ticket. If not, it'll have an error message.
+ */
 exports.createTicket = async (req, res) => {
   if (!req.auth) return res.status(401).json({ message: "Unauthorized." });
 
@@ -69,4 +80,37 @@ exports.createTicket = async (req, res) => {
         .status(400)
         .send(err ?? "An error has occurred when creating the ticket.");
     });
+};
+
+/**
+ * Updates an existing ticket.
+ * @param {*} req Contains the new properties of the ticket
+ * @param {*} res If updated successfully, returns the updated ticket.
+ */
+exports.updateTicket = async (req, res) => {
+  if (!req.auth) return res.status(401).json({ message: "Unauthorized." });
+
+  const ticketToUpdate = await Ticket.findById(res.body._id);
+
+  // Return 404 if the ticket could not be found
+  if (!ticketToUpdate)
+    return res.status(400).json({ message: "Ticket not found." });
+
+  // If the ticket is using a title that exists in the database already, return 400
+  Ticket.find({ title: res.body.title })
+    .then((ticket) => {
+      if (ticket) {
+        return res.status(400).json({
+          message:
+            "Ticket with the same title exists, please use a different title.",
+        });
+      }
+    })
+    .catch((err) => {
+      return res
+        .status(500)
+        .json({ message: err.message ?? "Could not update the ticket" });
+    });
+
+  await Ticket.findOneAndUpdate({ _id: ticketToUpdate._id }, {$set: {req.body}});
 };

a3d44148013072cc8598330d234973540a7e4036
Noah Chernet <noahchernet@yahoo.com>
2022-05-16 16:04:11 +0300

    postedByUser stores User model directly instead of email attachments decreased to 1 string, will be extended in the future in server/models/Ticket.js and RemovedTicket.js

--- server/models/RemovedTicket.js
+++ server/models/RemovedTicket.js
@@ -1,15 +1,16 @@
 const mongoose = require("mongoose");
 const commentSchema = require("./Comment").commentSchema;
+const userSchema = require("./User").userSchema;
 
 const ticketSchema = mongoose.Schema(
   {
     title: { type: String, required: true },
-    postedByUser: { type: String, required: true }, // user's email
+    postedByUser: { type: userSchema, required: true },
     description: { type: String, required: true },
     severity: { type: Number, required: true },
     comments: { type: [commentSchema] },
     solved: { type: Boolean, required: true },
-    attachments: { type: [] },
+    attachments: { type: String },
     solution: { type: [mongoose.Types.ObjectId] },
     due: { type: Date },
   },

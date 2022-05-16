b39e3e5327a5d0050bd343ad193bece680903ee4
Noah Chernet <noahchernet@yahoo.com>
2022-05-16 16:02:07 +0300

    Added userFromAuth function to create a user from the auth header in request models/User.js

--- server/models/User.js
+++ server/models/User.js
@@ -11,6 +11,20 @@ const userSchema = mongoose.Schema({
   picture: String, // url to image
 });
 
-let User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
 
-module.exports = User;
+/**
+ * Creates a user based on Auth0's auth header
+ * @param {*} req contains email, full name and profile picture of user
+ * @returns new User model based on Auth0's auth header
+ */
+const userFromAuth = (req) => {
+  return new User({
+    email: req.auth.email,
+    firstName: req.auth.given_name,
+    lastName: req.auth.family_name,
+    picture: req.auth.picture,
+  });
+};
+
+module.exports = { User, userSchema, userFromAuth };

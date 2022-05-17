e71651b86d1bf65c19877705a3e9c9fbc797b057
Noah Chernet <noahchernet@yahoo.com>
2022-05-17 09:57:53 +0300

    AUto collection creation for User modle disabled

--- server/models/User.js
+++ server/models/User.js
@@ -4,12 +4,15 @@ const mongoose = require("mongoose");
  * Defines what a User's attributes are in the mongo database.
  * devType: junior, mid-level, senior, tech lead, etc.
  */
-const userSchema = mongoose.Schema({
-  email: String,
-  firstName: String,
-  lastName: String,
-  picture: String, // url to image
-});
+const userSchema = mongoose.Schema(
+  {
+    email: String,
+    firstName: String,
+    lastName: String,
+    picture: String, // url to image
+  },
+  { autoCreate: false }
+);
 
 const User = mongoose.model("User", userSchema);
 

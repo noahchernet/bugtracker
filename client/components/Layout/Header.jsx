d0af9fb07dc4a85c54ba0ee1781f642ab84506db
Noah Chernet <noahchernet@yahoo.com>
2022-05-23 00:03:54 +0300

    Made header layout for entire webapp

--- client/components/Layout/Header.jsx
+++ client/components/Layout/Header.jsx
@@ -8,7 +8,7 @@ export function Header() {
     <div className="flex justify-between ">
       <Image className="w-2 h-2" src={logo} width={60} height={60} />
       <h1
-        className="my-2 mr-auto p-3  font-serif font-semi-bold
+        className="my-2 mr-auto p-3 font-serif font-semi-bold
         text-lg font-bold lg:text-4xl"
       >
         Avalon Bugtracker

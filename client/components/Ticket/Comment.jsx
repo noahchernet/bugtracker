7492526f05576ac844eb6eeeed7a4297d30e9788
Noah Chernet <noahchernet@yahoo.com>
2022-05-23 19:44:42 +0300

    Delete comment function for every comment

--- client/components/Ticket/Comment.jsx
+++ client/components/Ticket/Comment.jsx
@@ -7,8 +7,8 @@ const Comment = ({ comment_ }) => {
   const [comment, setComment] = useState(comment_);
   const [editing, setEditing] = useState(false);
   const [updatedComment, setUpdatedComment] = useState(comment_);
+  const [commentExists, setCommentExists] = useState(true);
   const { user } = useUser();
-  console.log("COmment:", comment);
 
   const handleInputChange = ({ name, value }) => {
     setUpdatedComment({ ...updatedComment, [name]: value });
@@ -16,77 +16,112 @@ const Comment = ({ comment_ }) => {
 
   const saveComment = async () => {
     const token = await axios.get("http://localhost:3000/api/getToken");
-    console.log(updatedComment);
     axios
       .put(
         "http://localhost:5000/comments/" + comment._id,
         { ...updatedComment },
         { headers: { Authorization: "Bearer " + token.data.token } }
       )
-      .then((res) => {
-        alert("Comment updated");
+      .then(() => {
         setComment(updatedComment);
+        console.log(`Comment ${comment._id} updated`);
       })
       .catch((err) => alert(err.response.data.message));
     comment.description;
   };
+
+  const deleteComment = async () => {
+    const token = await axios.get("http://localhost:3000/api/getToken");
+    const confirmDelete = confirm(
+      "Are you sure you want to delete this comment?"
+    );
+    console.log("Confirmatin:", confirmDelete);
+
+    if (confirmDelete) {
+      axios
+        .delete("http://localhost:5000/comments/" + comment._id, {
+          headers: { Authorization: "Bearer " + token.data.token },
+        })
+        .then((res) => {
+          console.log(res.data);
+          setCommentExists(false);
+        })
+        .catch((err) => alert(err.message));
+    }
+  };
+
   return (
-    <div className="flex mb-8 border-b-2 border-solid border-gray-200 py-4">
-      <div className="flex-col mr-5">
-        <Image
-          src={comment.postedByUser.picture}
-          width={"50px"}
-          height={"50px"}
-          className="rounded-full"
-        />
-        <p className="text-center">{comment.postedByUser.firstName}</p>
-      </div>
-      {editing ? (
-        <form>
-          <textarea
-            className="border-gray-300 border-2"
-            cols="80"
-            name="description"
-            value={updatedComment.description}
-            onChange={(e) =>
-              handleInputChange({ name: e.target.name, value: e.target.value })
-            }
-          />
-        </form>
-      ) : (
-        <p>{comment.description}</p>
-      )}
-      {user.sub === comment.postedByUser.sub && (
-        <>
+    <>
+      {commentExists ? (
+        <div className="flex mb-8 border-b-2 border-solid border-gray-200 py-4">
+          <div className="flex-col mr-5">
+            <Image
+              src={comment.postedByUser.picture}
+              width={"50px"}
+              height={"50px"}
+              className="rounded-full"
+            />
+            <p className="text-center">{comment.postedByUser.firstName}</p>
+          </div>
           {editing ? (
-            <>
-              <button
-                className="ml-auto rounded my-auto p-2 border-2"
-                onClick={() => {
-                  saveComment();
-                  setEditing(false);
-                }}
-              >
-                Save
-              </button>
-              <button
-                className="ml-auto rounded my-auto p-2 border-2"
-                onClick={() => setEditing(false)}
-              >
-                Cancel
-              </button>
-            </>
+            <form>
+              <textarea
+                className="border-gray-300 border-2"
+                cols="80"
+                name="description"
+                value={updatedComment.description}
+                onChange={(e) =>
+                  handleInputChange({
+                    name: e.target.name,
+                    value: e.target.value,
+                  })
+                }
+              />
+            </form>
           ) : (
-            <button
-              className="ml-auto rounded my-auto p-2 border-2"
-              onClick={() => setEditing(true)}
-            >
-              Edit
-            </button>
+            <p>{comment.description}</p>
           )}
-        </>
-      )}
-    </div>
+          {user.sub === comment.postedByUser.sub && (
+            <>
+              {editing ? (
+                <>
+                  <button
+                    className="ml-auto my-auto rounded p-2 border-2"
+                    onClick={() => {
+                      saveComment();
+                      setEditing(false);
+                    }}
+                  >
+                    Save
+                  </button>
+                  <button
+                    className="ml-auto rounded my-auto p-2 border-2"
+                    onClick={() => setEditing(false)}
+                  >
+                    Cancel
+                  </button>
+                </>
+              ) : (
+                <>
+                  <button
+                    className="ml-auto rounded my-auto p-2 border-2"
+                    onClick={() => setEditing(true)}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="ml-auto rounded my-auto p-2 border-2"
+                    onClick={() => deleteComment()}
+                  >
+                    Delete
+                  </button>
+                </>
+              )}
+            </>
+          )}
+        </div>
+      ) : null}
+    </>
   );
 };
 

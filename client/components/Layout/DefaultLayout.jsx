baef9b3befc04adb1f9bf04387143028ca56c72b
Noah Chernet <noahchernet@yahoo.com>
2022-05-30 17:48:00 +0300

    Footer added, links to my socials included

--- client/components/Layout/DefaultLayout.jsx
+++ client/components/Layout/DefaultLayout.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import Footer from "./Footer";
 import { Header } from "./Header";
 
 const DefaultLayout = (props) => {
   return (
     <div>
-      <Header />
-      {props.children}
+      <div className="pb-20">
+        <Header />
+        {props.children}
+      </div>
+      <Footer />
     </div>
   );
 };

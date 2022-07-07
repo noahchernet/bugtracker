7946b513418f81b9cbb6dbea456bd9dc94db7c39
Noah Chernet <noahchernet@yahoo.com>
2022-07-07 23:03:42 +0300

    using different cors middleware

--- server/server.js
+++ server/server.js
@@ -9,27 +9,20 @@ require("dotenv").config({ override: true });
 
 const app = express();
 
-const whitelist = [
-  "http://localhost:3000",
-  "https://avalon-bugtracker.vercel.app/",
-  "https://avalon-bugtracker-server.herokuapp.com/",
-];
+// const whitelist = [
+//   "http://localhost:3000",
+//   "https://avalon-bugtracker.vercel.app/",
+//   "https://avalon-bugtracker-server.herokuapp.com/",
+// ];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin
-      if (!origin) return callback(null, true);
-      if (whitelist.indexOf(origin) === -1) {
-        var message =
-          "The CORS policy for this origin doesn't " +
-          "allow access from the particular origin.";
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 // app.use(cors());
 app.use(formidableMiddleware());
 

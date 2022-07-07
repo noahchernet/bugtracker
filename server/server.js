0fd818acca8188eba57f73987fc64f80f1924c3c
Noah Chernet <noahchernet@yahoo.com>
2022-07-08 00:52:31 +0300

    cors usage set back to default, issue was with the frontend

--- server/server.js
+++ server/server.js
@@ -15,15 +15,15 @@ const app = express();
 //   "https://avalon-bugtracker-server.herokuapp.com/",
 // ];
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-// app.use(cors());
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+app.use(cors());
 app.use(formidableMiddleware());
 
 app.use("/users", userRouter);

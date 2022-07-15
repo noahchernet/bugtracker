a6bad20531dad36dcd94a41cc12c3740b972c77e
Noah Chernet <noahchernet@yahoo.com>
2022-07-16 02:22:21 +0300

    Delete button now works, deletes comment from database and removes comment from redux store

--- client/components/Comment.jsx
+++ client/components/Comment.jsx
@@ -3,10 +3,39 @@ import { Avatar, Box, Button, HStack, Text, VStack } from "@chakra-ui/react";
 import { useUser } from "@auth0/nextjs-auth0";
 import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
 import EditComment from "./EditComment";
+import { deleteOneComment } from "../features/commentListSlice/commentListSlice";
+import { useDispatch } from "react-redux";
+import axios from "axios";
 
 export default function Comment({ commentDetails }) {
   const { user } = useUser();
   const [editingComment, setEditingComment] = useState(false);
+  const dispatch = useDispatch();
+
+  const deleteComment = async () => {
+    const token = await axios.get(
+      `${process.env.NEXT_PUBLIC_CLIENT}/api/getToken`
+    );
+    const confirmDelete = confirm(
+      "Are you sure you want to delete this comment?"
+    );
+
+    if (confirmDelete) {
+      axios
+        .delete(
+          `${process.env.NEXT_PUBLIC_WEB_SERVER}/comments/` +
+            commentDetails._id,
+          {
+            headers: { Authorization: "Bearer " + token.data.token },
+          }
+        )
+        .then((res) => {
+          dispatch(deleteOneComment(commentDetails._id));
+        })
+        .catch((err) => alert(err.message));
+    }
+  };
+
   return (
     <HStack spacing="2.5rem" align="stretch" my="2rem" mb="6rem">
       <VStack spacing="1rem">
@@ -55,6 +84,7 @@ export default function Comment({ commentDetails }) {
                   color="white"
                   leftIcon={<DeleteIcon />}
                   _hover={{ bg: "red.600" }}
+                  onClick={deleteComment}
                 >
                   Delete
                 </Button>

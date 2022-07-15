52ce384d725c25a945ee50fbaf9b9e438e3ccdb4
Noah Chernet <noahchernet@yahoo.com>
2022-07-15 23:09:03 +0300

    Added EditComment component, to edit a selected comment. Triggered by clicking 'Edit' on a comment

--- client/components/Comment.jsx
+++ client/components/Comment.jsx
@@ -1,7 +1,12 @@
-import React from "react";
-import { Avatar, HStack, Text, VStack } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Avatar, Box, Button, HStack, Text, VStack } from "@chakra-ui/react";
+import { useUser } from "@auth0/nextjs-auth0";
+import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
+import EditComment from "./EditComment";
 
 export default function Comment({ commentDetails }) {
+  const { user } = useUser();
+  const [editingComment, setEditingComment] = useState(false);
   return (
     <HStack spacing="2.5rem" align="stretch" my="2rem" mb="6rem">
       <VStack spacing="1rem">
@@ -19,15 +24,45 @@ export default function Comment({ commentDetails }) {
             : commentDetails.postedByUser.email}
         </Text>
       </VStack>
-      <Text
-        bg="gray.100"
-        borderColor="gray.200"
-        borderWidth="0.1rem"
-        w="100%"
-        p="1rem"
-      >
-        {commentDetails.description}
-      </Text>
+      <Box width="100%">
+        {editingComment ? (
+          <EditComment
+            setEditingComment={setEditingComment}
+            commentToEdit={commentDetails}
+          />
+        ) : (
+          <>
+            <Text
+              bg="gray.100"
+              borderColor="gray.200"
+              borderWidth="0.1rem"
+              py="1rem"
+              px="0.5rem"
+            >
+              {commentDetails.description}
+            </Text>
+            {user && user.sub === commentDetails.postedByUser.sub ? (
+              <HStack pt="1rem" float="right">
+                <Button
+                  leftIcon={<EditIcon />}
+                  colorScheme="blue"
+                  onClick={() => setEditingComment(true)}
+                >
+                  Edit
+                </Button>
+                <Button
+                  bg="red.500"
+                  color="white"
+                  leftIcon={<DeleteIcon />}
+                  _hover={{ bg: "red.600" }}
+                >
+                  Delete
+                </Button>
+              </HStack>
+            ) : null}
+          </>
+        )}
+      </Box>
     </HStack>
   );
 }

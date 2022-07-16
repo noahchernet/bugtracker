aeb4b2f81a603835b98463def73f65a07c60260c
Noah Chernet <noahchernet@yahoo.com>
2022-07-17 00:39:30 +0300

    Removed unused slice ticketListLoadingSlice

--- client/app/store.js
+++ client/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import ticketListLoadingReducer from "../features/ticketListLoading/ticketListLoadingSlice";
 import commentListReducer from "../features/commentListSlice/commentListSlice";
 import ticketListReducer from "../features/ticketListSlice/ticketListSlice";
 
 export const store = configureStore({
   reducer: {
-    ticketListLoading: ticketListLoadingReducer,
     ticketList: ticketListReducer,
     commentList: commentListReducer,
   },

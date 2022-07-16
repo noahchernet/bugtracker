cfddc515956482fc850ff6628de2e522ce215350
Noah Chernet <noahchernet@yahoo.com>
2022-07-16 22:14:46 +0300

    using redux's ticketList state to render the ticket's list instead of react's state

--- client/pages/index.js
+++ client/pages/index.js
@@ -4,28 +4,27 @@ import Loading from "../components/Loading";
 import axios from "axios";
 import TicketBrief from "../components/TicketBrief";
 import { useSelector, useDispatch } from "react-redux";
-import { unsetLoading } from "../features/ticketListLoading/ticketListLoadingSlice";
+import { updateAllTickets } from "../features/ticketListSlice/ticketListSlice";
 
 export default function Home() {
-  const [allTickets, setAllTickets] = useState([]);
+  // const [allTickets, setAllTickets] = useState([]);
   // const [loading, setLoading] = useState(true);
-  const loading = useSelector((state) => state.ticketListLoading.value);
+  const allTickets = useSelector((state) => state.ticketList.ticketList);
   const dispatch = useDispatch();
 
   useEffect(() => {
     axios
       .get(`${process.env.NEXT_PUBLIC_WEB_SERVER}/tickets`, { params: "" })
       .then((response) => {
-        setAllTickets(response.data.reverse());
+        dispatch(updateAllTickets(response.data.reverse()));
         // console.log(allTickets);
       })
-      .catch((err) => console.log("Error fetching data\n", err))
-      .finally(dispatch(unsetLoading()));
-  }, [loading]);
+      .catch((err) => console.log("Error fetching data\n", err));
+  }, []);
 
   return (
     <Box as="section" pt="2rem">
-      {loading ? (
+      {allTickets === [] ? (
         <Loading />
       ) : (
         <VStack spacing={"1em"}>

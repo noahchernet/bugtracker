73f2714ff3f68bce5bfc369efb1addd13204136b
Noah Chernet <noahchernet@yahoo.com>
2022-07-17 19:51:43 +0300

    Using redux's ticketList (persisted with redux-persist) instead of fetching it again with axios
    Pasing the ticket loaded to child components so they don't fetch data again

--- client/pages/ticket/[id].jsx
+++ client/pages/ticket/[id].jsx
@@ -1,20 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import TicketDetails from "../../components/TicketDetails";
 import { Divider, VStack } from "@chakra-ui/react";
 import CommentsList from "../../components/CommentsList";
 import NewCommentDialog from "../../components/NewCommentDialog";
+import { useSelector } from "react-redux";
+import Loading from "../../components/Loading";
 
 export default function DetailedTicket() {
+  const allTickets = useSelector((state) => state.ticketList.ticketList);
+  // console.log("All tickets...", allTickets);
+  const [ticket, setTicket] = useState({}); // The ticket that this comment was added to
+  const [loading, setLoading] = useState(true);
   const router = useRouter();
 
-  useEffect(() => {}, [router.isReady]);
+  useEffect(() => {
+    if (router.isReady) {
+      setTicket(
+        allTickets.filter((ticket) => ticket._id === router.query.id)[0]
+      );
+      setLoading(false);
+    }
+  }, [router.isReady]);
 
-  return (
+  return loading && ticket === {} && router.query === {} ? (
+    <Loading />
+  ) : (
     <VStack pt="3rem">
-      <TicketDetails id={router.query.id} />
+      <TicketDetails ticket={ticket} />
       <Divider bg="gray.900" w="45%" />
-      <CommentsList ticketId={router.query.id} />
+      <CommentsList ticket={ticket} />
       <NewCommentDialog ticketId={router.query.id} />
     </VStack>
   );

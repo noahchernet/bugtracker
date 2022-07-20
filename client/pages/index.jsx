2c868e13a9e2e7654cb970aa3943c1e84ac2e4d6
Noah Chernet <noahchernet@yahoo.com>
2022-07-21 01:39:12 +0300

    Added parallax effect

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -8,6 +8,7 @@ import {
   Text,
 } from "@chakra-ui/react";
 import React from "react";
+import { Parallax, Background } from "react-parallax";
 
 const HomePageNavBar = () => {
   return (
@@ -18,6 +19,7 @@ const HomePageNavBar = () => {
       align="center"
       // bgGradient={"linear(to-tr, #89f7fe, #66a6ff)"}
       boxShadow="lg"
+      zIndex={2}
     >
       <HStack mr="-10rem">
         <Image
@@ -28,22 +30,22 @@ const HomePageNavBar = () => {
           Avalon Bugtracker
         </Heading>
       </HStack>
-      <Link>
+      <Link href="#">
         <Text color="#1852a5" fontSize="lg" fontFamily="Helvetica">
           Home
         </Text>
       </Link>
-      <Link>
+      <Link href="#features">
         <Text color="#1852a5" fontSize="lg" fontFamily="Helvetica">
           Features
         </Text>
       </Link>
-      <Link>
+      <Link href="#contactus">
         <Text color="#1852a5" fontSize="lg" fontFamily="Helvetica">
           Contact Us
         </Text>
       </Link>
-      <Link>
+      <Link href="/dashboard">
         <Text color="#1852a5" fontSize="lg" fontFamily="Helvetica">
           Get Started 🡲
         </Text>
@@ -52,10 +54,56 @@ const HomePageNavBar = () => {
   );
 };
 
+const Hero = () => {
+  return (
+    <Parallax
+      bgImage="https://res.cloudinary.com/dwzav7iui/image/upload/v1658354477/coding-picture-1_zpx3wj.jpg"
+      strength={800}
+      bgImageStyle={{ height: "80%" }}
+    >
+      {/* <Background>
+        <Image src="https://res.cloudinary.com/dwzav7iui/image/upload/v1658354477/coding-picture-1_zpx3wj.jpg" />
+      </Background> */}
+      <Box
+      // backgroundImage="url('https://res.cloudinary.com/dwzav7iui/image/upload/v1658354477/coding-picture-1_zpx3wj.jpg')"
+      // backgroundRepeat="repeat"
+      // backgroundSize="100%"
+      // backgroundPosition="fixed"
+      >
+        <Box py="30%">
+          <Heading
+            mt="15rem"
+            ml="5rem"
+            size="xl"
+            w="40%"
+            fontFamily="Helvetica"
+          >
+            Move fast, stay aligned, and build better - together
+            <br /> The best issue tracking tool used by developer teams
+          </Heading>
+        </Box>
+      </Box>
+    </Parallax>
+  );
+};
+
 export default function Home() {
   return (
     <>
       <HomePageNavBar />
+      <Hero />
+      <Text fontSize="lg">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </Text>
+      <Hero />
+      <Hero />
+      <Hero />
     </>
   );
 }

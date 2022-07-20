05b0ee0866198b10e9066e3f97eca9e2868a5dd0
Noah Chernet <noahchernet@yahoo.com>
2022-07-21 00:31:33 +0300

    Navbar added

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,8 +1,61 @@
-import React from 'react'
+import {
+  Image,
+  Heading,
+  Box,
+  Stack,
+  HStack,
+  Link,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
+
+const HomePageNavBar = () => {
+  return (
+    <Stack
+      direction={{ base: "column", md: "row" }}
+      justify="space-around"
+      py="1rem"
+      align="center"
+      // bgGradient={"linear(to-tr, #89f7fe, #66a6ff)"}
+      boxShadow="lg"
+    >
+      <HStack mr="-10rem">
+        <Image
+          boxSize="3rem"
+          src="https://res.cloudinary.com/dwzav7iui/image/upload/v1658078371/android-chrome-192x192_fg0f07.png"
+        />
+        <Heading as="h2" size="lg" fontWeight="light" fontFamily="Helvetica">
+          Avalon Bugtracker
+        </Heading>
+      </HStack>
+      <Link>
+        <Text color="#1852a5" fontSize="lg" fontFamily="Helvetica">
+          Home
+        </Text>
+      </Link>
+      <Link>
+        <Text color="#1852a5" fontSize="lg" fontFamily="Helvetica">
+          Features
+        </Text>
+      </Link>
+      <Link>
+        <Text color="#1852a5" fontSize="lg" fontFamily="Helvetica">
+          Contact Us
+        </Text>
+      </Link>
+      <Link>
+        <Text color="#1852a5" fontSize="lg" fontFamily="Helvetica">
+          Get Started 🡲
+        </Text>
+      </Link>
+    </Stack>
+  );
+};
 
 export default function Home() {
   return (
-    <div></div>
-  )
+    <>
+      <HomePageNavBar />
+    </>
+  );
 }
-

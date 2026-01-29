0ba646e51f4d3dde436edd2703d1d7e7d56567d6
Noah Chernet <86301024+noahchernet@users.noreply.github.com>
2026-01-29 13:45:31 +0300

    using nitor, and .output for build output

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,7 @@ import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   server: {
@@ -11,11 +12,8 @@ export default defineConfig({
   plugins: [
     tsConfigPaths(),
     tailwindcss(),
-    tanstackStart({
-      server: {
-        preset: "vercel",
-      },
-    }),
+    tanstackStart(),
+    nitro(),
     // react's vite plugin must come after start's vite plugin
     viteReact(),
   ],
